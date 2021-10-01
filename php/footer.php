2e55418ded48f640232a5e5244945f4a75fe1e16
ShadowShot07 <javier.vera.suarez380@gmail.com>
2021-10-01 17:46:51 +0200

    maquetado

---
+++ php/footer.php
@@ -0,0 +1,15 @@
+<?php ?>
+</div>
+<div>
+            <footer>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,32 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Home Page</title>
+</head>
+<body>
+<div class="container">
+        <header>
+            <div class="logo">
+
+            </div>
+            <div class="navbar">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div class="body-page">
